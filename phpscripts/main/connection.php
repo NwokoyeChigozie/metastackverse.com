fb61374b1047dbcca8e1424b9000012821947c42
gregoflash05 <gregoflash05@gmail.com>
2022-08-18 06:28:16 +0000

    project commit

---
+++ phpscripts/main/connection.php
@@ -0,0 +1,15 @@
+<?php
+//sql connection
+$link = mysqli_connect("localhost", "metaptno_metastack_adm", "cryptoservice", "metaptno_metastacktradesoptions");
+if(mysqli_connect_error()){
+    die('ERROR: Unable to connect:' . mysqli_connect_error());
+}
+
+
+//$link1 = mysqli_connect("localhost", "root", "", "skiphistory");
+//if(mysqli_connect_error()){
+//    die('ERROR: Unable to connect:' . mysqli_connect_error());
+//}
+    ?>
+
+
